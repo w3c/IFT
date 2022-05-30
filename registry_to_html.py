b496a3997d9b7c4f2a0e192ff12e7757c240ba70
Garret Rieger <grieger@google.com>
2022-05-30 22:52:52 +0000

    Update feature registry generation to handle cv01-cv99 automatically.

--- registry_to_html.py
+++ registry_to_html.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 print("<table>")
 print("  <tr><th>Tag</th><th>Encoded As</th></tr>")
@@ -6,11 +7,23 @@ with open("feature-registry.csv") as r:
   reader = csv.reader(r, delimiter=",")
   i = 1
   for row in reader:
-    if row[0] == "tag":
+    if row[0] == "tag" or row[0].startswith("#"):
         continue
-    v = "default" if row[2] == "1" else i
-    if row[2] != "1":
+
+
+    if row[2] == "1":
+      v = "default"
+    else:
+      m = re.search("[a-z]{2}([0-9]{2})-[a-z]{2}([0-9]{2})", row[0])
+      if m:
+        start = i
+        end = i + (int(m.group(2)) - int(m.group(1)))
+        i += end - start + 1
+        v = f"{start}-{end}"
+      else:
+        v = i
         i += 1
+
     print(f"  <tr><td>{row[0]}</td><td>{v}</td></tr>")
 
 print("</table>")
