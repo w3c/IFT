5ea0cf47c323a1f85e005faa7386cbd7b34a250e
Garret Rieger <grieger@google.com>
2023-06-20 23:24:07 +0000

    Revamp the FeatureTagSet encoding scheme.
    'default included' features can now be omitted from the set by including the ID associated with that feature. This allows the client to fully customize the feature set if the list of default included features does not match what the client's shaper is expected to require for it's current content. For example the 'vert' feature could be omitted if no vertical layout is being done.

--- registry_to_html.py
+++ registry_to_html.py
@@ -2,7 +2,7 @@ import csv
 import re
 
 print("<table>")
-print("  <tr><th>Tag</th><th>Encoded As</th></tr>")
+print("  <tr><th>Tag</th><th>Encoded As</th><th>Encode If</th></tr>")
 with open("feature-registry.csv") as r:
   reader = csv.reader(r, delimiter=",")
   i = 1
@@ -11,19 +11,20 @@ with open("feature-registry.csv") as r:
         continue
 
 
+    presence = "needed"
     if int(row[2]) == 1:
-      v = "default"
-    else:
-      m = re.search("[a-z]{2}([0-9]{2})-[a-z]{2}([0-9]{2})", row[0])
-      if m:
-        start = i
-        end = i + (int(m.group(2)) - int(m.group(1)))
-        i += end - start + 1
-        v = f"{start}-{end}"
-      else:
-        v = i
-        i += 1
+      presence = "not needed"
 
-    print(f"  <tr><td>{row[0]}</td><td>{v}</td></tr>")
+    m = re.search("[a-z]{2}([0-9]{2})-[a-z]{2}([0-9]{2})", row[0])
+    if m:
+      start = i
+      end = i + (int(m.group(2)) - int(m.group(1)))
+      i += end - start + 1
+      v = f"{start}-{end}"
+    else:
+      v = i
+      i += 1
+
+    print(f"  <tr><td>{row[0]}</td><td>{v}</td><td>{presence}</td></tr>")
 
 print("</table>")
