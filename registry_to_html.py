ab73e824b04b28286f80a7006ef7d4e3991b1813
Garret Rieger <grieger@google.com>
2023-06-20 23:46:32 +0000

    Reorder the feature tag list to have all of the 'not needed' features first as these are fixed and cannot be added to later on.

--- registry_to_html.py
+++ registry_to_html.py
@@ -5,26 +5,27 @@ print("<table>")
 print("  <tr><th>Tag</th><th>Encoded As</th><th>Encode If</th></tr>")
 with open("feature-registry.csv") as r:
   reader = csv.reader(r, delimiter=",")
+  lines = [row for row in reader]
   i = 1
-  for row in reader:
-    if row[0] == "Tag" or row[0].startswith("#"):
+  for is_default in [True, False]:
+    for row in lines:
+      if row[0] == "Tag" or row[0].startswith("#"):
         continue
 
+      if is_default != (int(row[2]) == 1):
+        continue
 
-    presence = "needed"
-    if int(row[2]) == 1:
-      presence = "not needed"
+      presence = "not needed" if int(row[2]) == 1 else "needed"
+      m = re.search("[a-z]{2}([0-9]{2})-[a-z]{2}([0-9]{2})", row[0])
+      if m:
+        start = i
+        end = i + (int(m.group(2)) - int(m.group(1)))
+        i += end - start + 1
+        v = f"{start}-{end}"
+      else:
+        v = i
+        i += 1
 
-    m = re.search("[a-z]{2}([0-9]{2})-[a-z]{2}([0-9]{2})", row[0])
-    if m:
-      start = i
-      end = i + (int(m.group(2)) - int(m.group(1)))
-      i += end - start + 1
-      v = f"{start}-{end}"
-    else:
-      v = i
-      i += 1
-
-    print(f"  <tr><td>{row[0]}</td><td>{v}</td><td>{presence}</td></tr>")
+      print(f"  <tr><td>{row[0]}</td><td>{v}</td><td>{presence}</td></tr>")
 
 print("</table>")
