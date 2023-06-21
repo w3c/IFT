29565790b1bb6a0c1a3637f8a3feaee393199024
Garret Rieger <grieger@google.com>
2023-06-21 00:16:00 +0000

    Introduce encoding version for the feature encoding registry.

--- registry_to_html.py
+++ registry_to_html.py
@@ -2,13 +2,19 @@ import csv
 import re
 
 print("<table>")
-print("  <tr><th>Tag</th><th>Encoded As</th><th>Encode If</th></tr>")
+print("  <tr><th>Tag</th><th>Encoded As</th><th>Encode If</th><th>Encoding Version</th></tr>")
 with open("feature-registry.csv") as r:
   reader = csv.reader(r, delimiter=",")
   lines = [row for row in reader]
+
   i = 1
   for is_default in [True, False]:
+    version = 1
     for row in lines:
+      if row[0].startswith("VERSION_2"):
+        version = 2
+        continue
+
       if row[0] == "Tag" or row[0].startswith("#"):
         continue
 
@@ -26,6 +32,6 @@ with open("feature-registry.csv") as r:
         v = i
         i += 1
 
-      print(f"  <tr><td>{row[0]}</td><td>{v}</td><td>{presence}</td></tr>")
+      print(f"  <tr><td>{row[0]}</td><td>{v}</td><td>{presence}</td><td>{version}</td></tr>")
 
 print("</table>")
